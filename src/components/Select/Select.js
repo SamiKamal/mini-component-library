4cc9eeda32081d3ee9f4a2791339ec2e2de113f7
SamiKamal <samixgames@hotmail.com>
2021-10-10 23:17:50 +0300

    Finish Select Challenge

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -7,12 +7,55 @@ import { getDisplayedValue } from './Select.helpers';
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
-
+  console.log(displayedValue)
   return (
-    <select value={value} onChange={onChange}>
+    <SelectWrapper>
+      {displayedValue}
+      <Label htmlFor={label}>
+        <Icon size="24" id="chevron-down"></Icon>
+      </Label>
+    <SelectOrg id={label} value={value} onChange={onChange}>
       {children}
-    </select>
+    </SelectOrg>
+    </SelectWrapper>
   );
 };
+
+const SelectWrapper = styled.div`
+  display: inline-block;
+  border: none;
+  position: relative;
+  width: 100%;
+  color: ${COLORS.gray700};
+  background-color: ${COLORS.transparentGray15};
+  max-width: fit-content;
+  padding: 12px 16px;
+  border-radius: 8px;
+
+  &:focus-within{
+    outline: 2px solid;
+    outline: 2px auto -webkit-focus-ring-color;
+  }
+
+  &:hover{
+    color: ${COLORS.black};
+  }
+`
+
+const SelectOrg = styled.select`
+    opacity: 0;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+`
+
+const Label = styled.label`
+  color: ${COLORS.grey50};
+  display: inline-block;
+  vertical-align: middle;
+  margin-left: 24px;
+`
 
 export default Select;
